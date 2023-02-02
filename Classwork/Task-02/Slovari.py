e19d8177c3809d1b7dd8054ad823228a280d3de0
immortalstr <immortalstr1990@mail.ru>
2023-02-02 16:22:27 +0300

    Add classwork

---
+++ Classwork/Task-02/Slovari.py
@@ -0,0 +1,20 @@
+# d = {}
+# d = dict()
+
+# d["q"] = "qwerty"
+# print(d)
+# d["w"] = 'warm'
+# print(d)
+# print(d["w"])
+# d[865] = 865.234
+# print(d)
+
+# # for item in d:
+# #     print(item,d[item])
+
+
+# print(d.items())
+# for (k, v) in d.items():
+#     print(k, v)
+
+
